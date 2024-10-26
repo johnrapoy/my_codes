39f3c9c92200ef2eb08b260109af1ddc7a5dfebb
DailyLollipops <clarencemadrigal84@gmail.com>
2024-10-27 02:08:06 +0800

    Update logic

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,35 +1,73 @@
 #include <Servo.h>
+#include "HX711.h"
 
-const int laserTransmitterPin = 3;
-const int laserReceiverPin = 4;
-const int ledPin = 5;
-const int buzzerPin = 6;
-const int proximitySensorPin = 7;
-const int servoPin = 8;
-const int irSensorPin = 9;
-const int stepperPulsePin = 10;
-const int stepperDirPin = 11;
-const int stepperEnaPin = 12; 
+const int stepperPulsePin = 2;
+const int stepperDirPin = 3;
+const int stepperEnaPin = 4; 
+const int laserTransmitterPin = 5;
+const int laserReceiverPin = 6;
+const int heightServoPin = 7;
+const int proximitySensorPin = 8;
+const int rejectServoPin = 9;
+const int irSensorFillingPin = 10;
+const int shredderRelay = 11;
+const int pneumaticRelay = 11;
+const int loadcellDoutPin = 12;
+const int loadcellSckPin = 13;
+const int irSensorCappingPin = 14;
+const int cappingRelay = 15;
+const int irSensorRejectedPin = 16;
 
 
-Servo servo;
+const int ledPin = 4;
+const int buzzerPin = 5;
 
+
+
+Servo heightServo;
+Servo rejectServo;
+HX711 scale;
+
+const double calibrationFactor = -7050;
 int currentCommand = -1;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(laserTransmitterPin, OUTPUT);
-  pinMode(laserReceiverPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
-  pinMode(proximitySensorPin, INPUT);
-  pinMode(irSensorPin, INPUT);
+
+  // Conveyor
   pinMode(stepperPulsePin, OUTPUT);
   pinMode(stepperDirPin, OUTPUT);
   pinMode(stepperEnaPin, OUTPUT);
 
+  // Height sensor
+  pinMode(laserTransmitterPin, OUTPUT);
+  pinMode(laserReceiverPin, INPUT);
   digitalWrite(laserTransmitterPin, HIGH);
-  servo.attach(servoPin);
+  heightServo.attach(heightServoPin);
+
+  // Plastic or can sensor
+  pinMode(proximitySensorPin, INPUT);
+  rejectServo.attach(rejectServoPin);
+
+  // Filling
+  pinMode(irSensorFillingPin, INPUT);
+  pinMode(shredderRelay, OUTPUT);
+  pinMode(pneumaticRelay, OUTPUT);
+  scale.begin(loadcellDoutPin, loadcellSckPin);
+  scale.set_scale(calibrationFactor);
+  scale.tare();
+  while (True){
+    if (scale.is_ready()) {
+      break;
+    }
+  }
+
+  // Capping
+  pinMode(irSensorCappingPin, INPUT);
+  pinMode(cappingRelay, OUTPUT);
+
+  // Rejected container
+  pinMode(irSensorRejectedPin, INPUT);
 }
 
 void loop() {
@@ -37,66 +75,122 @@ void loop() {
     receiveCommand();
   }
 
+  // Move conveyor starting
   else if (currentCommand == 0) {
-    // Detect from laser sensor
+    moveConveyorStart();
+    currentCommand = -1;
+  }
+
+  // Move conveyor mid
+  else if (currentCommand == 1) {
+    moveConveyorMid();
+    currentCommand = -1;
+  }
+
+  // Move conveyor ending
+  else if (currentCommand == 2) {
+    moveConveyorEnd();
+    currentCommand = -1;
+  }
+
+  // Detect from laser sensor
+  else if (currentCommand == 3) {
     bool detected = digitalRead(sensor);
     sendResponse(String(detected));
     currentCommand = -1;
   }
 
-  else if (currentCommand == 1) {
-    // Turn on LED
-    digitalWrite(ledPin, HIGH);
-    currentCommand = -1;
-  }
-
-  else if (currentCommand == 2) {
-    // Turn off LED
-    digitalWrite(ledPin, LOW);
-    currentCommand = -1;
-  }
-
-  else if (currentCommand == 3) {
-    // Turn on buzzer
-    tone(buzzerPin, 500);
-    currentCommand = -1;
-  }
-
+  // Turn on servo motor
   else if (currentCommand == 4) {
-    // Turn off buzzer
-    noTone(buzzerPin);
+    heightServo.write(180);
     currentCommand = -1;
   }
 
+  // Turn off servo motor
   else if (currentCommand == 5) {
-    // Detect from proximity sensor
+    heightServo.write(0);
+    currentCommand = -1;
+  }
+
+  // Detect from proximity sensor
+  else if (currentCommand == 6) {
     bool detected = digitalRead(proximitySensorPin);
     sendResponse(String(detected));
     currentCommand = -1;
   }
 
-  else if (currentCommand == 6) {
-    // Turn on servo motor
-    servo.write(180);
-    currentCommand = -1;
-  }
-
+  // Turn on servo motor
   else if (currentCommand == 7) {
-    // Turn off servo motor
-    servo.write(0);
+    rejectServo.write(180);
     currentCommand = -1;
   }
 
+  // Turn off servo motor
   else if (currentCommand == 8) {
-    // Detect from IR sensor
-    bool detected = digitalRead(irSensorPin);
+    rejectServo.write(0);
+    currentCommand = -1;
+  }
+
+  // Detect from IR sensor
+  else if (currentCommand == 9) {
+    bool detected = digitalRead(irSensorFillingPin);
     sendResponse(String(detected));
     currentCommand = -1;
   }
 
-  else if (currentCommand == 9) {
-    // Move conveyor
-    moveConveyor();
+  // Turn on shredder
+  else if (currentCommand == 10) {
+    digitalWrite(shredderRelay, HIGH);
+    currentCommand = -1;
+  }
+
+  // Turn off shredder
+  else if (currentCommand == 11) {
+    digitalWrite(shredderRelay, LOW);
+    currentCommand = -1;
+  }
+
+  // Turn on pneumatic actuator
+  else if (currentCommand == 12) {
+    digitalWrite(pneumaticRelay, HIGH);
+    currentCommand = -1;
+  }
+
+  // Turn off pneumatic actuator
+  else if (currentCommand == 13) {
+    digitalWrite(pneumaticRelay, LOW);
+    currentCommand = -1;
+  }
+
+  // Get weight
+  else if (currentCommand == 14) {
+    sendResponse(String((scale.get_units(), 1)));
+    currentCommand = -1;
+  }
+
+  // Detect from IR sensor
+  else if (currentCommand == 15) {
+    bool detected = digitalRead(irSensorCappingPin);
+    sendResponse(String(detected));
+    currentCommand = -1;
+  }
+
+  // Turn on capping relay
+  else if (currentCommand == 16) {
+    digitalWrite(cappingRelay, HIGH);
+    currentCommand = -1;
+  }
+
+  // Turn off capping relay
+  else if (currentCommand == 17) {
+    digitalWrite(cappingRelay, LOW);
+    currentCommand = -1;
+  }
+
+  // Detect from IR sensor
+  else if (currentCommand == 18) {
+    bool detected = digitalRead(irSensorRejectedPin);
+    sendResponse(String(detected));
     currentCommand = -1;
   }
 }
@@ -113,8 +207,32 @@ void sendResponse(String response) {
   Serial.println(response);    
 }
 
-void moveConveyor() {
-  for (int i=0; i<7500; i++) {
+void moveConveyorStart() {
+  for (int i=0; i<5000; i++) {
+    digitalWrite(stepperDirPin,LOW);
+    digitalWrite(stepperEnaPin,HIGH);
+    digitalWrite(stepperPulsePin,HIGH);
+    delayMicroseconds(50);
+    digitalWrite(stepperPulsePin,LOW);
+    delayMicroseconds(50);
+  }
+  sendResponse("done");
+}
+
+void moveConveyorMid() {
+  for (int i=0; i<6000; i++) {
+    digitalWrite(stepperDirPin,LOW);
+    digitalWrite(stepperEnaPin,HIGH);
+    digitalWrite(stepperPulsePin,HIGH);
+    delayMicroseconds(50);
+    digitalWrite(stepperPulsePin,LOW);
+    delayMicroseconds(50);
+  }
+  sendResponse("done");
+}
+
+void moveConveyorEnd() {
+  for (int i=0; i<6000; i++) {
     digitalWrite(stepperDirPin,LOW);
     digitalWrite(stepperEnaPin,HIGH);
     digitalWrite(stepperPulsePin,HIGH);
